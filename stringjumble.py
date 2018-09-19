f9c9e70b2d399f91089fc42d01d9d409ed9b65b0
megsnyder <42872006+megsnyder@users.noreply.github.com>
2018-09-19 08:51:53 -0400

    Updated from Brython Server: 9/19/2018 8:51:52 AM

--- stringjumble.py
+++ stringjumble.py
@@ -19,27 +19,6 @@ the user and prints it back in three different ways:
 
 Output of your program should look like this:
 
-"""
-stringjumble.py
-Author: Meg
-Credit: Noah
-
-Assignment:
-
-The purpose of this challenge is to gain proficiency with 
-manipulating lists.
-
-Write and submit a Python program that accepts a string from 
-the user and prints it back in three different ways:
-
-* With all letters in reverse.
-* With words in reverse order, but letters within each word in 
-  the correct order.
-* With all words in correct order, but letters reversed within 
-  the words.
-
-Output of your program should look like this:
-
 Please enter a string of text (the bigger the better): There are a few techniques or tricks that you may find handy
 You entered "There are a few techniques or tricks that you may find handy". Now jumble it:
 ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
@@ -51,7 +30,7 @@ print('You entered ''. Now jumble it:')
 letter = list(text)
 num = len(text)
 reverse = (letter[num::-1])
-word = (letter[0:num])
+word = (letter[:num:1])
 first = ""
 
 third = ""
@@ -59,7 +38,7 @@ fourth = ""
 
 sixth = ""
 seventh = ""
-a = 1
+a = 0
 b = -1
 for i in reverse:
     first += i
@@ -67,26 +46,21 @@ print(first)
 for i in reverse:
     a+=1
     if i == " ":
-        well = letter[num-(a-1):num-(b):]
+        well = letter[num-(a):num-(b):]
         b+=len(well)
         for i in well:
             third += i
-print(third)
 fine = letter[0:num-(b):]
 for i in fine:
     fourth += i
-print(fourth)
-a = 1
-b = -1
+print(third + " " + fourth)
+c = -1
+d = -1
 for i in word:
-    a+=1
+    c+=1
     if i == " ":
-        okay = letter[a:0:-1]
-        b+=len(okay)
+        okay = letter[c+(d):d:-1]
+        d+=1
         for i in okay:
             sixth += i
 print(sixth)
-sure = letter[a::-1]
-for i in sure:
-    seventh += i
-print(seventh)
